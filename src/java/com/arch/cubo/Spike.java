00c15ee7fd85d83bd271c385cf808c646a9135da
MrJoshTheEpic <35389597+MrJoshTheEpic@users.noreply.github.com>
2018-09-24 22:38:47 -0500

    Create Spike.java

---
+++ src/java/com/arch/cubo/Spike.java
@@ -0,0 +1,13 @@
+public class Spike extends GameObject{
+  
+  package com.arch.cubo;
+  
+  int amount;
+  
+  public Spike(int a, int b, int c, int d, int e){
+    super(a, b, c, d);
+    amount = e;
+  }
+}
+
+

8726b9ee384d464b58b774377f7cd5b1c47be569
MrJoshTheEpic <35389597+MrJoshTheEpic@users.noreply.github.com>
2018-09-24 22:30:56 -0500

    Create GameObject.java

---
+++ src/java/cubo/GameObject.java
@@ -0,0 +1,31 @@
+package cubo;
+
+public class GameObject{
+  
+  int x;
+  int y;
+  int l;
+  int h;
+  int t = 90;
+  
+  public GameObject(int x1, int y1, int l1, int h1){
+    x = x1;
+    y = y1;
+    l = l1;
+    h = h1;
+  }
+  
+  public GameObject(int x1, int y1){
+    x = x1;
+    y = y1;
+    l = 0;
+    h = 0;
+  }
+  
+  public void set(int x1, int y1){
+    x = x1;
+    y = y1;
+  }
+}
+
+

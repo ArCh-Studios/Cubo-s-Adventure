09664d0eb85c4d827d25da58b4ef1f4bc4b346ca
MrJoshTheEpic <35389597+MrJoshTheEpic@users.noreply.github.com>
2018-09-24 22:32:06 -0500

    Create Text.java

---
+++ src/java/cubo/Text.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+package cubo;
+
+public class Text extends GameObject{
+  
+  String text;
+  Color color;
+  float size;
+  
+  public Text(int x1, int y1, String s1, Color c1, float s2){
+    super(x1, y1);
+    text = s1;
+    color = c1;
+    size = s2;
+  }
+  
+  public Text(int x1, int y1, String s1, Color c1){
+    super(x1, y1);
+    text = s1;
+    color = c1;
+    size = 16;
+  }
+}
